2fc123bfbbcc676a4f3bd7e9bcdf38b4f1d87bd4
sadq <mamsa0205@gmail.com>
2024-07-20 15:27:35 +0330

    Phase01

---
+++ StarPhase01/StarPhase01/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Text.Json;
+using StarPhase01;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "data.json";
+        string jsonContent = File.ReadAllText(filePath);
+        var studentsList = JsonSerializer.Deserialize<List<Student>>(jsonContent);
+
+        filePath = "score.json";
+        jsonContent = File.ReadAllText(filePath);
+        var coursesList = JsonSerializer.Deserialize<List<Course>>(jsonContent);
+        foreach (var courseItem in coursesList)
+        {
+            Student selectedStudent = studentsList.Find(s => s.StudentNumber == courseItem.StudentNumber);
+            selectedStudent.addCourse(courseItem);
+        }
+
+        foreach (var student in getTopStudents(studentsList,3))
+        {
+            Console.WriteLine(student.ToString());
+        }
+    }
+
+    private static Student[] getTopStudents(List<Student> list, int a)
+    {
+        if (a > list.Count) a = list.Count;
+        Student[] result = new Student[a];
+        for (int i = 0; i < a; i++)
+        {
+            result[i] = new Student(0, "", "");
+        }
+        foreach (var student in list)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].getAverage()<student.getAverage())
+                {
+                    for (int j = result.Length-1; j > i; j--)
+                    {
+                        result[j] = result[j - 1];
+                    }
+
+                    result[i] = student;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ StarPhase01/StarPhase01/Student.cs
@@ -0,0 +1,59 @@
+namespace StarPhase01;
+
+public class Student
+{
+    private int _studentNumber;
+    private string _firstName;
+    private string _lastName;
+    private List<Course> _courses;
+    private double _scoresSum;
+    
+    public Student(int studentNumber, string firstName, string lastName)
+    {
+        _studentNumber = studentNumber;
+        _firstName = firstName;
+        _lastName = lastName;
+        _courses = new List<Course>();
+        _scoresSum = 0;
+    }
+
+    public int StudentNumber
+    {
+        get => _studentNumber;
+        set => _studentNumber = value;
+    }
+
+    public string FirstName
+    {
+        get => _firstName;
+        set => _firstName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set => _lastName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public List<Course> Courses
+    {
+        get => _courses;
+        set => _courses = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public override string ToString()
+    {
+        return _firstName + " " + _lastName + " " + getAverage();
+    }
+
+    public void addCourse(Course newCourse)
+    {
+        _courses.Add(newCourse);
+        _scoresSum += newCourse.Score;
+    }
+    public double getAverage()
+    {
+        return _courses.Count == 0 ? 0 : _scoresSum / _courses.Count;
+    }
+}
+

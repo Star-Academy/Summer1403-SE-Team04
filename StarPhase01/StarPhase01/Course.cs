d2a65bc46396f2381dad423c7a9cc3b3cef86146
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-07-21 15:42:06 +0330

    null

--- StarPhase01/StarPhase01/Course.cs
+++ StarPhase01/StarPhase01/Course.cs
@@ -5,7 +5,6 @@ public class Course
     public int StudentNumber { get; set; }
     public string Lesson{ get; set; }
     public double Score{ get; set; }
-
     public Course(int studentNumber, string lesson, double score)
     {
         StudentNumber = studentNumber;

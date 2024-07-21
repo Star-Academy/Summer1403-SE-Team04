6fe2a0e8ae8bb6a90e8def9143894a36e29391ef
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-07-21 11:33:38 +0330

    divide the code in main between input class and main

---
+++ StarPhase01/StarPhase01/Input.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace StarPhase01;
+
+public static class Input
+{
+    public static List<T> ReadStudentListFromJsonFile<T>(string jsonFilePath)
+    {
+        string jsonContent = File.ReadAllText(jsonFilePath);
+        return  JsonSerializer.Deserialize<List<T>>(jsonContent);
+    }
+    
+}
--- StarPhase01/StarPhase01/Program.cs
+++ StarPhase01/StarPhase01/Program.cs
@@ -1,30 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text.Json;
 using StarPhase01;
 
 class Program
 {
     static void Main()
     {
-        string filePath = @"..\..\..\database\data.json";
-        string jsonContent = File.ReadAllText(filePath);
-        var studentsList = JsonSerializer.Deserialize<List<Student>>(jsonContent);
-
-        filePath = @"..\..\..\database\score.json";
-        jsonContent = File.ReadAllText(filePath);
-        var coursesList = JsonSerializer.Deserialize<List<Course>>(jsonContent);
+        string nameDataFilePath = @"..\..\..\database\data.json";
+        string scoreDstaFilePath = @"..\..\..\database\score.json";
+        var studentsList = Input.ReadStudentListFromJsonFile<Student>(nameDataFilePath);
+        var coursesList = Input.ReadStudentListFromJsonFile<Course>(scoreDstaFilePath);
         foreach (var courseItem in coursesList)
         {
             Student selectedStudent = studentsList.Find(s => s.StudentNumber == courseItem.StudentNumber);
             selectedStudent.AddCourse(courseItem);
         }
-
-        foreach (var student in TopStudents.GetTopStudents(studentsList,3))
-        {
-            Console.WriteLine(student.ToString());
-        }
+         string output =String.Join("\n", TopStudents.GetTopStudents(studentsList, 3).Select(s => s.ToString()));
+         Console.WriteLine(output);
     }
-
-    
 }

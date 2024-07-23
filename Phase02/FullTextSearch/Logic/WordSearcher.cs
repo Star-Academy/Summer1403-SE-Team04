a5e6a7848d174dd6f2b6d2bfed3cee139e30fcd4
sadq <mamsa0205@gmail.com>
2024-07-23 12:01:37 +0330

    Edit method readability

--- Phase02/FullTextSearch/Logic/WordSearcher.cs
+++ Phase02/FullTextSearch/Logic/WordSearcher.cs
@@ -1,52 +1,1 @@
-using System.Runtime.CompilerServices;
-
-namespace FullTextSearch;
-
-public class WordSearcher
-{
-    public InvertedIndex Index;
-
-    public WordSearcher(InvertedIndex invertedIndex)
-    {
-        Index = invertedIndex;
-    }
-
-    public List<string> FindDocuments(string query)
-    {
-        var words= query.Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(w => w.FixWordFormat());
-        
-        var mustExist = words.Where(word => !word.StartsWith('+') && !word.StartsWith('-'))
-            .Select(FindWordInDocuments)
-            .ToList()
-            .Intersect();
-        
-        var mustNotExist = words.Where(word => word.StartsWith('-'))
-            .Select(word => FindWordInDocuments(word.Substring(1)))
-            .ToList()
-            .Union();
-        
-        var atLeastOneExists = words.Where(word => word.StartsWith('+'))
-            .Select(word => FindWordInDocuments(word.Substring(1)))
-            .ToList()
-            .Union();
-        
-        
-        if (!mustExist.Any()) return atLeastOneExists.Except(mustNotExist).ToList();
-        else if (!atLeastOneExists.Any()) return mustExist.Except(mustNotExist).ToList();
-        else return mustExist.Except(mustNotExist).Except(mustExist.Except(atLeastOneExists)).ToList();
-    }
-    
-    private List<string> FindWordInDocuments(string word)
-    {
-        try
-        {
-            var result = Index.InvertedIndexMap.GetValueOrDefault(word);
-            return result==null?new List<string>():result;
-        }
-        catch (NullReferenceException r)
-        {
-            Console.WriteLine(r);
-            throw;
-        }
-    }
-}
+using System.Runtime.CompilerServices;namespace FullTextSearch;public class WordSearcher{    public InvertedIndex Index;    public WordSearcher(InvertedIndex invertedIndex)    {        Index = invertedIndex;    }    public List<string> FindDocuments(string query)    {        var words= query.Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(w => w.FixWordFormat());                var mustExist = words.Where(word => !word.StartsWith('+') && !word.StartsWith('-'))            .Select(FindWordInDocuments)            .ToList()            .Intersect();                var mustNotExist = words.Where(word => word.StartsWith('-'))            .Select(word => FindWordInDocuments(word.Substring(1)))            .ToList()            .Union();                var atLeastOneExists = words.Where(word => word.StartsWith('+'))            .Select(word => FindWordInDocuments(word.Substring(1)))            .ToList()            .Union();                        if (!mustExist.Any()) return atLeastOneExists.Except(mustNotExist).ToList();        else if (!atLeastOneExists.Any()) return mustExist.Except(mustNotExist).ToList();        else return mustExist.Except(mustNotExist).Except(mustExist.Except(atLeastOneExists)).ToList();    }        private List<string> FindWordInDocuments(string word)    {        try        {            var result = Index.InvertedIndexMap.GetValueOrDefault(word);            return result==null?new List<string>():result;        }        catch (NullReferenceException r)        {            Console.WriteLine(r);            throw;        }    }}

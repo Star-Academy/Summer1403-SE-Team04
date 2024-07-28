fa2b93891add25b25569f313612924cd6d69f92e
sadq <mamsa0205@gmail.com>
2024-07-28 11:02:26 +0330

    Fix naming convention of Resources.resx

--- Phase03/FullTextSearch/Controllers/Logic/SmallWordsRemover.cs
+++ Phase03/FullTextSearch/Controllers/Logic/SmallWordsRemover.cs
@@ -5,7 +5,7 @@ namespace FullTextSearch.Controllers.Logic;
 
 public static class SmallWordsRemover
 {
-    private static readonly IEnumerable<String> SmallWordsList = FileReader.FileReaderInstance.Read(Resources.smallWordsPath);
+    private static readonly IEnumerable<String> SmallWordsList = FileReader.FileReaderInstance.Read(Resources.SmallWordsPath);
     public static IEnumerable<string> RemoveSmallWords(this IEnumerable<string> list)
     {
         return list.Where(word => !SmallWordsList.Contains(word));

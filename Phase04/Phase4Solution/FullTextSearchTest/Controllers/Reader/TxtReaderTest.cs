f471fdec59d4ca6ea8642dc0c18fa22fe4ef612c
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-07-31 12:14:40 +0330

    fix the address of path the file

--- Phase04/Phase4Solution/FullTextSearchTest/Controllers/Reader/TxtReaderTest.cs
+++ Phase04/Phase4Solution/FullTextSearchTest/Controllers/Reader/TxtReaderTest.cs
@@ -4,20 +4,35 @@ namespace FullTextSearchTest.Controllers.Reader;
 
 public class TxtReaderTest
 {
+    private TxtReader _sut;
+
+    public TxtReaderTest()
+    {
+        _sut = new TxtReader();
+    }
+
     [Theory]
     [InlineData(null)]
     [InlineData("")]
+
     public void Read_ShouldBeStringEmpty_IfPathIsNullOrEmpty(string path)
     {
-        Assert.Equal(new TxtReader().Read(path), new List<string>());
+        //arrange 
+        var result = new List<string>();
+        //act
+        var check = _sut.Read(path);
+        //assert
+        Assert.Equal(result,check);
     }
 
     [Fact]
     public void Read_ShouldBeSpilitedList_IfPathIsNormal()
     {
-        Assert.Equal(
-            new TxtReader().Read(
-                "D:\\Desktop\\programing\\C#\\Summer1403-SE-Team04\\Phase04\\Phase4Solution\\FullTextSearchTest\\AssetTest\\TxtReadFileTest.txt"),
-            new[] { "ali", "reza" });
+        //arrange 
+        var result = new[] { "ali", "reza" };
+        //act
+        var check = _sut.Read("AssetTest\\TxtReadFileTest.txt");
+        //assert
+        Assert.Equal(result, check);
     }
 }

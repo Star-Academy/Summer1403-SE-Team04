3fb756f7061467a25401cef828762a307264a9b9
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-08-03 18:14:19 +0330

    add advance quryhandler

--- Phase04/Phase4Solution/FullTextSearch/Controllers/Logic/StringProcessor/QueryHandler.cs
+++ Phase04/Phase4Solution/FullTextSearch/Controllers/Logic/StringProcessor/QueryHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FullTextSearch.Controllers.Logic.Abstraction;
 using FullTextSearch.Controllers.Logic.DocumentsLoader;
 
@@ -12,5 +13,37 @@ public static class QueryHandler
             .FixWordsList(reformaters);
         var enumerableWords = words as string[] ?? words.ToArray();
         return enumerableWords;
+    }
+
+    public static string[] SplitIntoAdvanceFormattedWords(this string query, List<IStringReformater> reformaters)
+    {
+        if (string.IsNullOrEmpty(query)) return [];
+
+        var result = new List<string>();
+        
+        var matches = Regex.Matches(query, @"([+-]?[^\s""']+)|""([^""]*)""");
+
+        foreach (Match match in matches)
+        {
+            if (match.Groups[2].Success)
+            {
+                if(match.Groups[2].Value is "+" or "-")continue;
+                int index = query.IndexOf(match.Value);
+                if (index > 0 && (query[index - 1] == '+' || query[index - 1] == '-'))
+                {
+                    result.Add(query[index - 1] + match.Groups[2].Value);
+                }
+                else
+                {
+                    result.Add(match.Groups[2].Value);
+                }
+            }
+            else
+            {
+                if(match.Value is "+" or "-")continue;
+                result.Add(match.Value);
+            }
+        }
+        return result.ToArray();
     }
 }

609d0c3f5166f22cca697ca70f205b912fca8df6
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-08-06 14:56:50 +0330

    add main

--- Phase06/SearchAPI/SearchAPI/Program.cs
+++ Phase06/SearchAPI/SearchAPI/Program.cs
@@ -1,4 +1,20 @@
+using FullTextSearch.Controllers;
+using FullTextSearch.Controllers.Logic;
+using FullTextSearch.Controllers.Logic.Creator_Loader;
+using FullTextSearch.Controllers.Logic.DocumentsLoader;
+using FullTextSearch.Controllers.Reader;
 using Microsoft.OpenApi.Models;
+
+
+var docCatcher = new DocCatcher();
+var advIndexcatcher = new AdvanceInvertedIndexCatcher();
+var docLoader = new DocumentLoader(new DocBuilder(new TxtReader(), docCatcher), new SmallWordsRemover());
+var indicesList = new List<string> { Resources.DocumentsPath };
+// var indexCreator = new InvertedIndexCreator(cacher, docLoader);
+var inputListener = new CliInputListener(advIndexcatcher, docCatcher);
+var outputPrinter = new OutputPrinter();
+new ServiceStartupInitializer(inputListener, outputPrinter,
+    new AdvanceInvertedIndexCreator(docCatcher, advIndexcatcher, docLoader)).Init(indicesList);
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen(c =>
